adbb902d9bd8b27e0c6272bd4d648890d210c96e
Joao Costa <joaopedrocosta.contato@gmail.com>
2023-08-05 22:28:36 -0300

    (strapi) remove files

--- app/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '22a2f762afeccb622e8f12a1f9c280c3'),
-    },
-  },
-});
